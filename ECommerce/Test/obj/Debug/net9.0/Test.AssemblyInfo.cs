81d38b3bb45cbf78efbfc15d9e6c7e258a362959
sejalkulkarni <sejalkulkarni017@gmail.com>
2026-02-11 18:19:47 +0530

    razor pages added

--- ECommerce/Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
+++ ECommerce/Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad2a2affffb6bf857455eb63e40211acbbf4bd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3fc71d18ebd281feb433ca2c1f3feb4d89ce5e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
